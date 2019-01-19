0ad97a44b844bc24aa8399ace9ef4500e2d8bb68
grigorasalex <alex_grigoras_10@yahoo.com>
2019-01-19 18:03:58 +0200

    Completed phase 1: each process processes each file and displays the word frequency; added leader choice to select the process with the highest time

--- MapReduce_MPI/MapReduce_MPI/main.cpp
+++ MapReduce_MPI/MapReduce_MPI/main.cpp
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <fstream>
 #include <dirent.h> 
+#include <list>	
+#include <ctime>
 #include "mpi.h"
 
 using namespace std;
@@ -23,7 +25,23 @@ using namespace std;
 #define FILE_TXT 32768
 #define ROOT 0
 #define NAME_SIZE 261
-#define FILE_PATH "test_files/"
+#define EMPTY_CHAR ""
+#define R_CHOICE 1					
+#define R_LEADER 2
+#define S_LEADER 1
+#define S_NONLIDER 0
+
+struct words
+{
+	char *word;
+	int frequency;
+};
+
+void appendChar(char* s, char c) {
+	int len = strlen(s);
+	s[len] = c;
+	s[len + 1] = '\0';
+}
 
 void getFileNames(const char *path)
 {
@@ -32,7 +50,9 @@ void getFileNames(const char *path)
 	int tag = 0;								// tag for communication
 	DIR *d;										//
 	struct dirent *dir;							//
-	char fileName[NAME_SIZE];					// stores file name
+	char *fileName;								// stores file name
+
+	fileName = (char*)malloc(NAME_SIZE * sizeof(char));
 
 	d = opendir(path);
 	if (d)
@@ -50,18 +70,91 @@ void getFileNames(const char *path)
 	}
 	else
 	{
-		printf("Could not open current directory");
+		printf("> Could not open current directory");
+		exit(EXIT_FAILURE);
 	}
 }
-/*
-void readWords(FILE *f) 
+
+list<words> readWords(FILE *fp)
 {
-	char x[1024];
-	// assumes no word exceeds length of 1023
-	while (fscanf(f, " %1023s", x) == 1) {
-		puts(x);
+	char c;
+	bool nextWord = false;
+	char word[1024] = "";
+	list<words> wordList;
+
+	while ((c = fgetc(fp)) != EOF)
+	{
+		if ((c == ' ' || c == '\n') && !nextWord)
+		{
+			// create new word
+			nextWord = true;
+			words wordVar;
+			wordVar.word = (char*)malloc(NAME_SIZE * sizeof(char));
+			strcpy_s(wordVar.word, strlen(word) + 1, word);
+			wordVar.frequency = 1;
+
+			// check if it exists
+			list<words>::iterator ListItem;
+			bool elemExists = false;
+			for (ListItem = wordList.begin(); ListItem != wordList.end(); ++ListItem)
+			{
+				if (strcmp(ListItem->word, wordVar.word) == 0)
+				{
+					ListItem->frequency++;
+					elemExists = true;
+				}
+			}
+
+			// add it in list if it doesn't exists
+			if (!elemExists)
+			{
+				wordList.push_back(wordVar);
+			}
+
+			// empty read word
+			strcpy_s(word, strlen(EMPTY_CHAR) + 1, EMPTY_CHAR);
+		}
+		else if(c >= -1 && c <= 255 && isalpha(c))
+		{ 
+			nextWord = false;
+			appendChar(word, tolower(c));
+		}
 	}
-}*/
+
+	return wordList;
+}
+
+void display_words(char *fileName, list<words> wordList, bool sort = false)
+{
+	// sort words
+	if(sort)
+	{
+		wordList.sort([](const words &f, const words &s) {
+			if (strcmp(f.word, s.word) < 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		});
+	}
+	
+	// display words
+	printf("<%s,{", fileName);
+	for (std::list<words>::iterator it = wordList.begin(); it != wordList.end(); ++it)
+	{
+		printf("%s:%d", it->word, it->frequency);
+		++it;
+		if (it != wordList.end())
+		{
+			printf(",");
+		}
+		--it;
+	}
+	printf(">\n");
+}
 
 int main(int argc, char* argv[]) {
 	/// Variables
@@ -69,26 +162,124 @@ int main(int argc, char* argv[]) {
 	int count;									// number of processes  
 	int tag = 0;								// tag for communication
 	MPI_Status status;							// return status for receive 
-	char message[NAME_SIZE];					// message buffer for receive
-	char filePath[NAME_SIZE+12];				//
-	char folder[] = "test_files/\0";				//
+	char *message;								// message buffer for receive
+	char *filePath;								//
 	FILE * fp;									//
+	errno_t err;
+	///
+	int nrDimensions = 2;					// plasa de procesoare are 1 dimensiune
+	int nrElemDim = NR_PROCESSES;			// numarul de elemente pe fiecare dimensiune in parte = NP
+	int dims[NR_PROCESSES] = { 1, NR_PROCESSES };	// elementele din dimensiune
+	int periods[] = { 1, 1 };				// periodicitatea este pe ambele dimensiuni
+	int gasitLider = false;
+	int statute = S_LEADER;					// initial, fiecare proces considera ca e lider (ca e cel mai bun)
+	int ID;									// ID-ul fiecarui proces
+	int left;								// vecinii procesului curent
+	int right;
+	int round = R_CHOICE;					// tag for messages
+	int vectorRanks[NR_PROCESSES];
+	MPI_Comm commCart;						// construim o topologie de tip cartezian
+	///
+	double elapsedSecs = 0.0;
+	double sendMessage;
+	double receiveMessage;
 
 	/// MPI initialization
 	MPI_Init(&argc, &argv);						// start up MPI
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);		// find out process rank
 	MPI_Comm_size(MPI_COMM_WORLD, &count);		// find out number of processes
+	MPI_Cart_create(MPI_COMM_WORLD, nrDimensions, dims, periods, 1, &commCart);
+	MPI_Comm_rank(commCart, &myRank);			// aflam rank-ul procesului curent
+	MPI_Comm_size(commCart, &count);			// aflam numarul de procese
 
+	for (int i = 0; i < 26; i++)
+	{
+		vectorRanks[i] = i;
+	}
+
+	MPI_Scatter(vectorRanks, 1, MPI_INT, &ID, 1, MPI_INT, ROOT, commCart);
+
+	MPI_Cart_shift(commCart, 1, 1, &left, &right);
+
+	message = (char*)malloc(NAME_SIZE*sizeof(char));			
+	filePath = (char*)malloc(NAME_SIZE * sizeof(char));
+
+	// if the process is ROOT
 	if (myRank == ROOT) {
 		getFileNames(DIR_NAME);					// get file names and number of them
 	}
+	// if the process is worker
 	else {
+		// receive file name
 		MPI_Recv(message, strlen(message) + 1, MPI_CHAR, ROOT, tag, MPI_COMM_WORLD, &status);
-		strcat_s(filePath, NAME_SIZE, folder);
-		//strcat_s(message, sizeof(message) + 1, filePath);
-		printf("[%d] received file name: \%s\n", myRank, filePath);
-		//fp = fopen(, "w+");
+		snprintf(filePath, strlen(DIR_NAME) + 1 + strlen(message) + 1, "%s%c%s", DIR_NAME, '/', message);
+		//printf(" [%d] received file name: %s\n", myRank, filePath);
+
+		// open file with received name
+		err = fopen_s(&fp, filePath, "r");
+		if (err)
+		{
+			perror("> Error while opening the file.\n");
+			exit(EXIT_FAILURE);
+		}
+		// measure time
+		clock_t begin = clock();
+
+		// list of words
+		list<words> wordList;
+		wordList = readWords(fp);
+	
+		//display_words(message, wordList);
+
+		// get elapsed time
+		clock_t end = clock();
+		elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+		//printf("Process %d finished in %lf seconds", myRank, elapsedSecs);
 	}
+
+	sendMessage = elapsedSecs;
+	
+
+	MPI_Send(&sendMessage, 1, MPI_INT, right, round, commCart);
+
+	while (!gasitLider)
+	{
+		MPI_Recv(&receiveMessage, 1, MPI_INT, left, MPI_ANY_TAG, commCart, &status);
+
+		switch (status.MPI_TAG)
+		{
+		case R_CHOICE:
+			if (receiveMessage == sendMessage)
+			{
+				statute = S_LEADER;
+				round = R_LEADER;
+				MPI_Send(&sendMessage, 1, MPI_INT, right, round, commCart);
+				gasitLider = true;
+			}
+			else if (receiveMessage > sendMessage)
+			{
+				MPI_Send(&receiveMessage, 1, MPI_INT, right, round, commCart);
+			}
+			break;
+		case R_LEADER:
+			statute = S_NONLIDER;
+			round = R_LEADER;
+			MPI_Send(&receiveMessage, 1, MPI_INT, right, round, commCart);
+			gasitLider = true;
+			break;
+		}
+	}
+
+	if (statute == S_LEADER)
+	{
+		printf("> Process[%d] TOOK THE MOST TIME: %lf\n", myRank, sendMessage);
+	}
+	else 
+	{
+		printf("> Process[%d] took: %lf\n", myRank, sendMessage);
+	}
+
+	MPI_Comm_free(&commCart);
 
 	/// MPI finalization
 	MPI_Finalize();								// shut down MPI
